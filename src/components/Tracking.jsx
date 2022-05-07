4379272e68497545b4d8e27ee8e34a08307307b2
Daniel Borini <dsouzaborini@gmail.com>
2022-05-07 17:39:22 -0300

    add extras to validation

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -12,6 +12,30 @@ class Tracking extends Component {
     };
   }
 
+  filterExtras(extras) {
+    const keys = Object.keys(extras);
+    const filteredExtrasKeys = keys.filter((key) => !key.includes('#'));
+    const filteredExtrasValues = filteredExtrasKeys.map((key) => extras[key]);
+    const filteredExtrasObject = filteredExtrasKeys.reduce((obj, key, i) => {
+      obj[key] = filteredExtrasValues[i];
+      return obj;
+    }, {});
+
+    return filteredExtrasObject;
+  }
+
+  renderExtras(extras) {
+    const filteredExtrasObject = this.filterExtras(extras);
+    return Object.keys(filteredExtrasObject).map((key) => (
+      <p key={ key } className="tc__extras">
+        { key }
+        :
+        {' '}
+        { filteredExtrasObject[key] }
+      </p>
+    ));
+  }
+
   renderTracking() {
     const { jsonData } = this.props;
 
@@ -22,11 +46,13 @@ class Tracking extends Component {
     }
 
     return jsonData.map((tracking, i) => {
-      const { category, action } = tracking.parsedSettings;
+      const { category, action, extras } = tracking.parsedSettings;
+
       return (
         <div key={ i } className="tc__div">
           <p className="tc__title">{ category }</p>
           <p className="tc__subtitle">{ action }</p>
+          { this.renderExtras(extras) }
         </div>
       );
     });
